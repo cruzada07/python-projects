ae2cee012db6b08c3ef792168a0bf0806f645ed0
Kevin <kevindelacruzsanchez596@gmail.com>
2026-09-12 10:52:47 -0500

    feat: integrar lista Tuple

--- modulo1/tmp2.py
+++ modulo1/tmp2.py
@@ -1,6 +1,9 @@
 # estructuras de datos
 
+#   --------------------------------------------------------------------
+
 # 1. List
+
 numeros = [10, 20, 30]
 
 print(numeros[0])   #10
@@ -13,4 +16,22 @@ print(frutas)
 
 #   ¿Cuando usar List?
 #       Cuando tienes una colección de elementos que puede cambiar
-#   -> list = colección ordenada que puede cambiar
+#   -> List = Colección ordenada que puede cambiar
+
+#   --------------------------------------------------------------------
+
+# 2. Tuple
+
+nms = (10, 20, 30)
+
+print(nms[2])
+#   NO SE PUEDE MODIFICAR -> nms[0] = 100  X
+punto = (10,20)
+dias = ("lunes", "martes", "miercoles","jueves")
+
+#   ¿Cuando usar Tuple?
+#       Cuando tienes un conjunto de datos que NO deberia modificarse
+#   -> Tuple = Una lista que NO puedes modificar
+
+#   --------------------------------------------------------------------
+
